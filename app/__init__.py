415a451ec8649ad567e7af19ecc1a4102bb99dbd
viktorija.g <grietniece2003@gmail.com>
2025-05-18 22:40:35 +0300

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,23 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
 # Importē Flask klasi, lai varētu izveidot web aplikācijas objektu
 from flask import Flask 
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Inicializē Flask aplikāciju — šis ir centrālais app objekts visam projektam
 app = Flask(__name__)
 
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 # Importē routes.py, lai Flask zina par visiem definētajiem maršrutiem
 from app import routes
+
+with app.app_context():
+    db.create_all()
